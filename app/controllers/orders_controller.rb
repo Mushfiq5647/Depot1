ac2c80ebae8bdb2530225f9dcb63eedef62ba129
Mushfiq5647 <mushfiqfcc56@gmail.com>
2021-01-05 18:05:24 +0600

    Internationalization Added

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,11 @@
-
+#---
+# Excerpted from "Agile Web Development with Rails 5",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/rails5 for more book information.
+#---
 class OrdersController < ApplicationController
   skip_before_action :authorize, only: [:new, :create]
   include CurrentCart
@@ -37,8 +44,8 @@ class OrdersController < ApplicationController
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
         OrderMailer.received(@order).deliver_later
-        format.html { redirect_to store_index_url, notice:
-          'Thank you for your order.' }
+        format.html { redirect_to store_index_url(locale: I18n.locale),
+          notice: I18n.t('.thanks') }
         format.json { render :show, status: :created,
           location: @order }
       else
